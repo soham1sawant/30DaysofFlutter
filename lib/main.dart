9648d03241a2680a99cb280413c778e85a1b249a
Soham Sawant <geekysoham@gmail.com>
2021-02-18 19:09:07 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:welcome_app/pages/home_page.dart';
 import 'package:welcome_app/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:welcome_app/utils/routes.dart';
+import 'package:welcome_app/widgets/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,13 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
